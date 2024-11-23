839377016f48fbc5752bc8d14f66795c081bcec9
Mehmet <mathdenizi@gmail.com>
2024-11-23 03:18:44 +0100

    front end created with expo

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const { PORT } = require("../config/keys");
 
-const port = process.env.PORT || 4000;
+const port = PORT || 4000;
 
 app.listen(port, () => console.log(`Listening on ${port}`));
